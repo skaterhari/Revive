a300601a6f4d775173f2f72698e27695fc2e6e48
skaterhari <111996528+skaterhari@users.noreply.github.com>
2022-12-06 20:52:52 +0530

    Add files via upload

--- wb.js
+++ wb.js
@@ -1,13 +1,13 @@
 function todark(){
     let lg=document.querySelector('#moon');
+    var bd=document.querySelector('body');
+    bd.classList.toggle('darkmode1');
     if(lg.src.match('black-crescent-moon-png-2.png'))
     {
-        var bd=document.querySelector('body');
-        bd.classList.toggle('darkmode1');
-        lg.src='bright.png';
+        lg.src='./images/bright.png';
     }
     else{ 
-        lg.src='./black-crescent-moon-png-2.png';
+        lg.src='./images/black-crescent-moon-png-2.png';
     }
    
 }
